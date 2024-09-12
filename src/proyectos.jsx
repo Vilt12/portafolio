f3a9917bb4c374bc595c37468410fe7b4c5000e3
Vilt12 <lutrax777@gmail.com>
2024-09-12 14:26:22 -0300

    Cambios

--- src/proyectos.jsx
+++ src/proyectos.jsx
@@ -36,11 +36,11 @@ function Proyectos() {
          <a href="https://tienda-lovat.vercel.app/"><img src={"https://cdn-icons-png.flaticon.com/128/869/869636.png"} alt=""/></a>
          <h4 >Tienda-Ecommerce</h4>
             </div>
-            
+           
            <div className="seccion-proyectos-tarjeta">
     
-         <a href="https://foro-gold.vercel.app/"><img src={"https://globalitec.gt/web/image/78729/fkmjededijkfijkg.jpg"} alt=""/></a>
-         <h4 >Tienda-Ecommerce</h4>
+         <a id="Carta-F" href="https://foro-gold.vercel.app/"><img src={"https://cdn-icons-png.flaticon.com/512/9826/9826013.png"} alt=""/></a>
+         <h4 >Fosforito</h4>
             </div>
         </div>
         </div>
