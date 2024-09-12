2b65cd257650f00b4a132ce29fba1b4e086d2e3c
Vilt12 <lutrax777@gmail.com>
2024-09-12 14:05:59 -0300

    Se agrego otra tarjeta de proyecto

--- src/proyectos.jsx
+++ src/proyectos.jsx
@@ -36,6 +36,12 @@ function Proyectos() {
          <a href="https://tienda-lovat.vercel.app/"><img src={"https://cdn-icons-png.flaticon.com/128/869/869636.png"} alt=""/></a>
          <h4 >Tienda-Ecommerce</h4>
             </div>
+            
+           <div className="seccion-proyectos-tarjeta">
+    
+         <a href="https://foro-gold.vercel.app/"><img src={"https://globalitec.gt/web/image/78729/fkmjededijkfijkg.jpg"} alt=""/></a>
+         <h4 >Tienda-Ecommerce</h4>
+            </div>
         </div>
         </div>
         </>
